cef88bc6af3085fe35ba090ce3a46ea2a6946e54
Andy Ealovega <andyblarblar@gmail.com>
2020-10-22 23:38:20 -0400

    run service as user to allow for network drive use,

--- SyncSharpWorker/installScripts/installService.ps1
+++ SyncSharpWorker/installScripts/installService.ps1
@@ -1,3 +1,15 @@
+Function Set-ServiceAcctCreds([string]$strCompName,[string]$strServiceName,[string]$newAcct,[string]$newPass){
+  $filter = 'Name=' + "'" + $strServiceName + "'" + ''
+  $service = Get-WMIObject -ComputerName $strCompName -namespace "root\cimv2" -class Win32_Service -Filter $filter
+  $service.Change($null,$null,$null,$null,$null,$null,$newAcct,$newPass)
+  $service.StopService()
+  while ($service.Started){
+    sleep 2
+    $service = Get-WMIObject -ComputerName $strCompName -namespace "root\cimv2" -class Win32_Service -Filter $filter
+  }
+  $service.StartService()
+}
+
 #Service needs absolute path
 $binPath = Resolve-Path ".\SyncSharpWorker\SyncSharpWorker.exe"
 
@@ -9,3 +21,10 @@ $params = @{
   Description = "Background worker for SyncSharp."
 }
 New-Service @params
+
+$name = Read-Host 'What is your domain username? This is used to allow the service to see network drives. Put .\Username for accounts on this PC'
+
+$pass = Read-Host 'What is your password?'
+
+#run the 
+Set-ServiceAcctCreds -strCompName "." -strServiceName "SyncSharp" -newAcct $name -newPass $pass
--- SyncSharpWorker/installScripts/uninstallService.ps1
+++ SyncSharpWorker/installScripts/uninstallService.ps1
@@ -1,1 +1,6 @@
 sc.exe delete "SyncSharp"
+
+ $strServiceName = "SyncSharp"
+ $filter = 'Name=' + "'" + $strServiceName + "'" + ''
+ $service = Get-WMIObject -ComputerName "." -namespace "root\cimv2" -class Win32_Service -Filter $filter
+ $service.StopService()
